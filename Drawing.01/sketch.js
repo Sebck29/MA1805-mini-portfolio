20ea296f05e5bf084cf071899b621f1fcd0c546f
Seb CK <Sebcassonkinch@gmail.com>
2025-10-12 15:45:08 +0100

    works now with a cicle

---
+++ Drawing.01/sketch.js
@@ -0,0 +1,14 @@
+function setup() {
+  createCanvas(500, 500); // canvas width/height
+  background(0); // Make background black
+}
+
+// Start draw function code block
+function draw(){ 
+  fill(255);    // Set fill color
+  ellipse(250, 250, 200, 200); 
+} 
+// End of the code block 
+ 
+ 
+  
