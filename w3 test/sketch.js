1d4d094cf5ab0cdef40963f7e7fe03df8d6ac534
Sebck29 <sebcassonkinch@gmail.com>
2025-10-13 11:32:44 +0100

    Add files via upload

---
+++ w3 test/sketch.js
@@ -0,0 +1,38 @@
+let house = 33;
+var mouse = 66;
+let hot = true
+let temp = 55.88;
+let idontknow;
+let red;
+let green;
+let blue;
+let size = 20;
+let name = "Seb"
+
+
+function setup() {
+  let mywidth = 200
+  createCanvas (mywidth, mywidth);
+  red = random (0, 255)
+  green = random (0, 255)
+  blue = random (0, 255)
+  let person = 55;
+
+
+}
+  
+function draw()  {
+
+  background(220)
+  fill( red, green, blue)
+
+//circle(50, 50, 50)
+circle(blue, red, size);
+size = size+ 0
+text(name, 100, 100)
+// Text attributes
+fill(150, size, 100)
+
+}
+
+
