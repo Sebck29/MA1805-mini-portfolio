0ea3291a52f450f7da8e41e49d97a7d22f667306
Sebck29 <sebcassonkinch@gmail.com>
2025-10-18 16:26:48 +0100

    Add files via upload

---
+++ Movement.02/sketch.js
@@ -0,0 +1,33 @@
+function setup() {
+  createCanvas(640, 480); 
+  background(230);    
+  // noStroke();              
+}
+
+function draw(){
+    fill(0); 
+    rect(0, 0, 100, 100); // x, y, w, h
+    
+    
+    stroke(20, 200, 200);  // r, g, b 
+strokeWeight(0);      // Width of stroke
+fill(200, 20, 200);    // r, g, b
+    fill(64);
+		rect(50, 50, 100, 100); 
+
+		fill(128)
+		rect(100, 100, 100, 100);
+		
+		fill(196);
+		rect(150, 150, 200, 200);
+
+		fill(220)
+		rect(300, 300, 400, 400); 
+
+   strokeWeight(20)
+    fill (255, 255, 0) //r, g, b
+   circle(200, 50, 200)
+
+   strokeWeight (10)
+   fill(0, 255, 0)
+}
